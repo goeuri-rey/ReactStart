b45637c161d46aa102451eb37516387be5762ef3
goeuri-rey <115942541+goeuri-rey@users.noreply.github.com>
2022-11-21 16:31:54 +0900

    2022/11/21 React機能追加学習⑧~⑨

--- src/index.js
+++ src/index.js
@@ -121,10 +121,10 @@
 // console.log(arr7);
 
 /**
- * mapやfillterを使った配列の処理
+ * ⑦mapやfillterを使った配列の処理
  */
-const nameArr = ["田中", "山田", "レイ"];
-// for(let index = 0; index < nameArr.length; index++){
+// const nameArr = ["田中", "山田", "レイ"];
+// for (let index = 0; index < nameArr.length; index++) {
 //   console.log(`${index + 1}番名は${nameArr[index]}です`);
 // }
 
@@ -133,10 +133,64 @@ const nameArr = ["田中", "山田", "レイ"];
 // })
 // console.log(nameArr2);
 
-nameArr.map((name, index) => console.log(name));
+// nameArr.map((name, index) => console.log(name));
 
 // const numArr = [1, 2, 3, 4, 5];
 // const newNumArr = numArr.filter((num) => {
 //   return num % 2 === 1;
 // })
 // console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "レイ") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+/**
+ * ⑧三項演算子
+ */
+//ある条件 ? 条件がtrueの持：じょうけんがfalseの時
+// const val1 = 1 > 0 ? `trueです`:`falseです` ;
+// console.log(val1);
+
+// const num = 1300;
+//toLocaleString : 3桁区切りに,を付けてくれる
+// console.log(num.toLocaleString());
+
+// const formattedNm = typeof num === 'number'? num.toLocaleString() : '数値を入力してください。';
+// console.log(formattedNm);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています！" : `許容範囲内です`;
+// };
+
+// console.log(checkSum(50, 40));
+
+/**
+ * ⑨論理演算子の本当の意味を知ろう　&& ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+//実は'または'とか'かつ'の意味ではない？
+// || は左側がfalseなら右を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
